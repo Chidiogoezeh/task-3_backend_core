4c3992fc4c478e60bc8ae0a3556bea32e5fa023e
Chidiogo <chidiogoezeh.me@gmail.com>
2026-04-29 09:14:34 +0100

    fix up

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Health Check: /api/profiles (Version 1 Required)`);
   console.log(`Server running on UTC: ${new Date().toISOString()}`);
-  console.log(`Endpoint: http://localhost:${PORT}/api/profiles`);
 });
